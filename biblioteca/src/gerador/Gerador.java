c8b9c2c4ea837d062cf8932ebe7d1cc0a90b24a9
Felipe Ticiani <felipe.ticiani@alunos.sc.senac.br>
2022-07-28 21:56:50 -0300

    Gerador

--- biblioteca/src/gerador/Gerador.java
+++ biblioteca/src/gerador/Gerador.java
@@ -10,5 +10,33 @@ package gerador;
  * @author felipe.ticiani
  */
 public class Gerador {
+    public static String gerarNumero(int qtd) {
+        String numero = "";
+        for (int i = 0; i < qtd; i++) {
+            numero += (int) (Math.random() * 10);
+        }
+        return numero;
+    }
     
+    public static String gerarTelefoneFixo() {
+        return "(48)3" + gerarNumero(3) + "-" + gerarNumero(4);
+    }
+    
+    public static String gerarTelefoneCelular() {
+        return "(48)99" + gerarNumero(3) + "-" + gerarNumero(4);
+    }
+    
+    public static String gerarCpf() {
+        return gerarNumero(3) + "." 
+                + gerarNumero(3) + "."
+                + gerarNumero(3) + "-"
+                + gerarNumero(2);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(gerarNumero(3));
+        System.out.println(gerarTelefoneFixo());
+        System.out.println(gerarTelefoneCelular());
+        System.out.println(gerarCpf());
+    }
 }
