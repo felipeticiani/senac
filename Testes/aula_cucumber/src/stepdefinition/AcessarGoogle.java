1c6499781b4af772b87cabc9629feae4bed690c9
Felipe Ticiani <felipe.ticiani@alunos.sc.senac.br>
2022-10-26 21:41:23 -0300

    aula 26/10/2022

---
+++ Testes/aula_cucumber/src/stepdefinition/AcessarGoogle.java
@@ -0,0 +1,37 @@
+package stepdefinition;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class AcessarGoogle {
+    @Dado("que o usuário está com o navegador aberto")
+    public void que_o_usuário_está_com_o_navegador_aberto() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Dado("que o usuário acessou a URL www.google.com.br")
+    public void que_o_usuário_acessou_a_URL_www_google_com_br() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Dado("que o usuário pesquisou por Faculdade Senac")
+    public void que_o_usuário_pesquisou_por_Faculdade_Senac() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Quando("clicar no link Senac Santa Catarina")
+    public void clicar_no_link_Senac_Santa_Catarina() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Então("o site da Faculdade Senac deve ser aberto")
+    public void o_site_da_Faculdade_Senac_deve_ser_aberto() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
---
+++ Testes/aula_cucumber/src/util/Componentes.java
@@ -0,0 +1,18 @@
+package util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Componentes {
+    private WebDriver driver;
+
+    public void inicializa() {
+        String chromeDriver = System.getProperty("user.dir") + "/Driver/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromeDriver);
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("file:///" + System.getProperty("user.dir") + "/Driver/componentes.html");
+    }
+}
